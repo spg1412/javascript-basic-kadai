5191c3577af0d8ec04325dacba9ad8283a11b06a
spg1412 <terransullivan@hotmail.com>
2024-07-17 18:01:35 +0900

    Update mdn.js

--- kadai_00x/mdn.js
+++ kadai_00x/mdn.js
@@ -1,9 +1,9 @@
 const today = new Date();
 
-const year = today.getFullYear() -1; // Getting the Year
+const year = today.getFullYear(); // Getting the Year
 
-const month = today.getMonth() +2; // Getting the Month, adding +1 because the month starts from the number 0
+const month = today.getMonth() +1; // Getting the Month, adding +1 because the month starts from the number 0
 
-const day = today.getDate() +5; // Getting the Date (day)
+const day = today.getDate(); // Getting the Date (day)
 
 console.log(year + '年' + month + '月' + day + '日');
