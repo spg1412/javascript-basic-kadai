09e18001e1f07327bef6e3f9fcc2acef46e6ec76
spg1412 <terransullivan@hotmail.com>
2024-07-17 18:41:54 +0900

    kadai_014

---
+++ kadai_014/square.js
@@ -0,0 +1,31 @@
+// Testing functions with Squares
+// function nearestSq(n){
+//     let numberToSquare = 1;
+//     let squared = Math.pow(numberToSquare, 2);
+//     while (square <n ) {
+//         numberToSquare += 1;
+//         squared = Math.pow(numberToSquare, 2);
+//     }
+//     const prevSquared = Math.pow(numberToSquare-1, 2);
+//     const diff = Math.abs(n - squared);
+//     const prevDiff = Math.abs(n - prevSquard);
+//     if (diff > prevDiff) return prevSquared;
+//     return squared;
+// }
+
+// Descriptions to the assignment
+//Define a function that multiplies the given argument num and prints the value
+const addnum = (num) => {
+    //console.log(num*10);
+  }
+  // Call a function and pass a number as an argument
+  //addnum(10);
+  
+  // Description found in search *console tab*
+  const square = (num) => {
+    return num ** 2;
+};
+  //Setting the number to 100
+  const result = square(10);
+  console.log(result);
+  
